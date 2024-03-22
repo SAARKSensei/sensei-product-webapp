facdeb3997069d99b1b3d63b43446c860e290f09
Shreyas Thakare <shreyasthakare5@gmail.com>
2024-03-22 16:30:17 +0530

    [ST] 22/3/24 Leftside corrections

--- src/Components/LeftSide.js
+++ src/Components/LeftSide.js
@@ -13,19 +13,14 @@ const LeftSide = () => {
         alt="background served with static path of image folder in src directory"
         className="h-screen	w-screen"
       />
-      <div>
-        <div className="absolute top-36 left-44 h-3/5 border-8 rounded-md	 border-cyan-400 bg-slate-200 w-80">
-          {/* <Image
-     src={Fram}
-     className='h-1/4	 w-72' 
-     /> */}
-        </div>
-        <div className="backgroud-color absolute h-72 w-52 top-52 right-28">
-          <h5 className="font-semibold	text-white	px-7 pt-7">
+      <div className='absolute top-0 left-0 w-full h-screen flex items-center justify-center'>
+        <div className="h-3/5 border-8 rounded-md	 border-cyan-400 bg-slate-200 w-80"></div>
+        <div className="backgroud-color h-72 w-52 -ml-8 relative">
+          <h5 className="font-semibold text-white	px-7 pt-7">
             Lorem ipsum dolor sit amet consectetur. Nisi lectus eget elit montes
             turpis velit.
           </h5>
-          <Image src={Doctor} alt='doctor pic' className="h-28 w-28 stikey mt-16 -ml-9" />
+          <Image src={Doctor} alt='doctor pic' className="h-28 w-28 mt-[70px] -ml-9" />
           <div className="h-28 w-40 absolute left-20 top-40 mt-5 bg-[#FFF0DE]">
             <h5 className="font-sans  px-2 font-bold">Dr. Aprajita Dixit</h5>
             <p className="text-xs	px-2 pt-1 ">
